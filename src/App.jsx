fc9d6cf36b35b213c1dc98637ab43f2ffaf64dc3
247626 Baska Dawid <247626@edu.p.lodz.pl>
2026-03-10 21:48:05 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainPage from "./components/MainPage.jsx";
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
---
+++ src/components/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MainPage(){
+
+    return (
+        <div>
+            <p className="text-5xl text-center" >Main page!</p>
+        </div>
+    )
+}
+
+export default MainPage;
